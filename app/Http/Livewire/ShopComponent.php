21afa887ce06496d335e8e5991232c007c2763dc
Chandan-Ojha <chandanojha776@gmail.com>
2021-02-21 00:09:34 +0600

    Making shop page products dynamic

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ShopComponent extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.shop-component')->layout("layouts.base");
+        $products = Product::paginate(12);
+        return view('livewire.shop-component', ['products' => $products])->layout("layouts.base");
     }
 }
