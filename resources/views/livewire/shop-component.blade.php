236c0991137669ad33e4be038101ba41e1473a92
Chandan-Ojha <chandanojha776@gmail.com>
2021-03-01 13:58:21 +0600

    Create Product Details Page

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -65,12 +65,12 @@
                         <li class="col-lg-4 col-md-6 col-sm-6 col-xs-6 ">
                             <div class="product product-style-3 equal-elem ">
                                 <div class="product-thumnail">
-                                    <a href="detail.html" title="{{$product->name}}">
+                                    <a href="{{route('product.details',['slug'=>$product->slug])}}" title="{{$product->name}}">
                                         <figure><img src="{{ asset('assets/images/products') }}/{{$product->image}}" alt="{{$product->name}}"></figure>
                                     </a>
                                 </div>
                                 <div class="product-info">
-                                    <a href="#" class="product-name"><span>{{$product->name}}</span></a>
+                                    <a href="{{route('product.details',['slug'=>$product->slug])}}" class="product-name"><span>{{$product->name}}</span></a>
                                     <div class="wrap-price"><span class="product-price">{{$product->regular_price}}</span></div>
                                     <a href="#" class="btn add-to-cart">Add To Cart</a>
                                 </div>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use Illuminate\Support\Facades\Route;
 
@@ -34,6 +35,7 @@ Route::get('/cart', CartComponent::class);
 
 Route::get('/checkout', CheckoutComponent::class);
 
+Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 
 /*
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
