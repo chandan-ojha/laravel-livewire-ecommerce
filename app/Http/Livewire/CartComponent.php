4163319aaf0f2e31f7312732d221905c4308a69d
Chandan-Ojha <chandanojha776@gmail.com>
2021-03-12 23:39:14 +0600

    update cart quantity

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,9 +3,24 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Cart;
 
 class CartComponent extends Component
 {
+    public function increaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty + 1;
+        Cart::update($rowId, $qty);
+    }
+
+    public function decreaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty - 1;
+        Cart::update($rowId, $qty);
+    }
+
     public function render()
     {
         return view('livewire.cart-component')->layout("layouts.base");
