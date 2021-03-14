481f58ea2418d7081deceb00aeaca50b114c1098
Chandan-Ojha <chandanojha776@gmail.com>
2021-03-14 19:49:48 +0600

    product sorting

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -9,6 +9,15 @@ use Cart;
 
 class ShopComponent extends Component
 {
+    public $sorting;
+    public $pagesize;
+
+    public function mount()
+    {
+        $this->sorting = "default";
+        $this->pagesize =12;
+    }
+
     public function store($product_id, $product_name, $product_price)
     {
         Cart::add($product_id, $product_name, 1, $product_price)->associate('App\Models\Product');
@@ -18,7 +27,26 @@ class ShopComponent extends Component
     use WithPagination;
     public function render()
     {
-        $products = Product::paginate(12);
+        if($this->sorting=='date')
+        {
+          $products = Product::orderBy('created_at','DESC')->paginate($this->pagesize);
+        }
+
+        else if($this->sorting=="price")
+        {
+         $products = Product::orderBy('regular_price','ASC')->paginate($this->pagesize);   
+        }
+
+        else if($this->sorting=="price-desc")
+        {
+          $products = Product::orderBy('regular_price','DESC')->paginate($this->pagesize);  
+        }
+        else
+        {
+            $products = Product::paginate($this->pagesize); 
+        }
+      
         return view('livewire.shop-component', ['products' => $products])->layout("layouts.base");
     }
 }
+
