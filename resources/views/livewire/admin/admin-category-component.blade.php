cf7c71f71a7f679b6ab5e77aea0d0c9678b1485e
Chandan-Ojha <chandanojha776@gmail.com>
2021-03-15 20:23:10 +0600

    admin edit category

--- resources/views/livewire/admin/admin-category-component.blade.php
+++ resources/views/livewire/admin/admin-category-component.blade.php
@@ -37,7 +37,9 @@
                                 	<td>{{$category->id}}</td>
                                 	<td>{{$category->name}}</td>
                                 	<td>{{$category->slug}}</td>
-                                	<td></td>
+                                	<td>
+                                     <a href="{{route('admin.editcategory',['category_slug'=>$category->slug])}}"> <i class="fa fa-edit fa-2x"> </i> </a>   
+                                    </td>
                                 </tr>
     					  	  @endforeach
     					  </tbody>	
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
+use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
@@ -63,7 +64,9 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 
 //For Admin
 Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function () {
-    Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
-    Route::get('/admin/categories',AdminCategoryComponent::class)->name('admin.categories');
-    Route::get('/admin/category/add',AdminAddCategoryComponent::class)->name('admin.addcategory');
+ Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+ Route::get('/admin/categories',AdminCategoryComponent::class)->name('admin.categories');
+ Route::get('/admin/category/add',AdminAddCategoryComponent::class)->name('admin.addcategory');
+ Route::get('/admin/category/edit/{category_slug}',AdminEditCategoryComponent::class)->name('admin.editcategory');
+
 });
