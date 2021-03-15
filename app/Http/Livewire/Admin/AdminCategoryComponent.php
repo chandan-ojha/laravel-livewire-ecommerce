212be73616d8775a8f48a1e93c702b3c4d9b8aa2
Chandan-Ojha <chandanojha776@gmail.com>
2021-03-15 20:59:58 +0600

    admin delete category

--- app/Http/Livewire/Admin/AdminCategoryComponent.php
+++ app/Http/Livewire/Admin/AdminCategoryComponent.php
@@ -9,6 +9,14 @@ use Livewire\WithPagination;
 class AdminCategoryComponent extends Component
 {
 	use WithPagination;
+
+	public function deleteCategory($id)
+	{
+      $category = Category::find($id);
+      $category->delete();
+      session()->flash('message','Category has been deleted successfully!');
+	}
+
     public function render()
     {
     	$categories = Category::paginate(5);
--- resources/views/livewire/admin/admin-category-component.blade.php
+++ resources/views/livewire/admin/admin-category-component.blade.php
@@ -22,6 +22,9 @@
     					</div>
     				</div>
     				<div class="panel-body">
+                        @if(Session::has('message'))
+                          <div class="alert alert-success" role="alert">{{Session::get('message')}} </div>
+                        @endif
     					<table class="table table-striped">
     					  <thead>
     					  	<tr>
@@ -38,7 +41,11 @@
                                 	<td>{{$category->name}}</td>
                                 	<td>{{$category->slug}}</td>
                                 	<td>
-                                     <a href="{{route('admin.editcategory',['category_slug'=>$category->slug])}}"> <i class="fa fa-edit fa-2x"> </i> </a>   
+
+                                     <a href="{{route('admin.editcategory',['category_slug'=>$category->slug])}}"> <i class="fa fa-edit fa-2x"> </i> </a>
+
+                                     <a href="#" wire:click.prevent="deleteCategory({{$category->id}})" style="margin-left:10px;"> <i class="fa fa-times fa-2x text-danger"> </i> </a>  
+
                                     </td>
                                 </tr>
     					  	  @endforeach
