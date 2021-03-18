543ff56418290401e6a9c7e09461a344ca9cc27d
Chandan-Ojha <chandanojha776@gmail.com>
2021-03-18 20:54:37 +0600

    admin making on sale carousel dynamic

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -12,13 +12,13 @@ class HomeComponent extends Component
 {
     public function render()
     {
-    	$sliders = HomeSlider::where('status',1)->get();
-    	$lproducts = Product::orderBy('created_at','DESC')->get()->take(8);
-    	$category = HomeCategory::find(1);
+        $sliders = HomeSlider::where('status', 1)->get();
+        $lproducts = Product::orderBy('created_at', 'DESC')->get()->take(8);
+        $category = HomeCategory::find(1);
         $cats = explode(',', $category->sel_categories);
-        $categories = Category::whereIn('id',$cats)->get();
+        $categories = Category::whereIn('id', $cats)->get();
         $no_of_products = $category->no_of_products;
-        return view('livewire.home-component',['sliders'=>$sliders,'lproducts'=>$lproducts,
-        'categories'=>$categories,'no_of_products' => $no_of_products])->layout('layouts.base');
+        $sproducts = Product::where('sale_price', '>', 0)->inRandomOrder()->get()->take(8);
+        return view('livewire.home-component', ['sliders' => $sliders, 'lproducts' => $lproducts, 'categories' => $categories, 'no_of_products' => $no_of_products, 'sproducts' => $sproducts])->layout('layouts.base');
     }
 }
--- resources/views/livewire/details-component.blade.php
+++ resources/views/livewire/details-component.blade.php
@@ -1,5 +1,13 @@
 <main id="main" class="main-site">
-
+    <style>
+        .regprice {
+            font-weight: 300;
+            font-size: 13px !important;
+            color: #aaaaaa !important;
+            text-decoration: line-through;
+            padding-left: 10px;
+        }
+    </style>
     <div class="container">
 
         <div class="wrap-breadcrumb">
@@ -38,7 +46,14 @@
                         <div class="wrap-social">
                             <a class="link-socail" href="#"><img src="{{ asset('assets/images/social-list.png') }}" alt=""></a>
                         </div>
-                        <div class="wrap-price"><span class="product-price">{{$product->regular_price}}</span></div>
+                        @if($product->sale_price > 0)
+                        <div class="wrap-price">
+                            <span class="product-price">${{$product->sale_price}}</span>
+                            <del><span class="product-price regprice">${{$product->regular_price}}</span></del>
+                        </div>
+                        @else
+                        <div class="wrap-price"><span class="product-price">${{$product->regular_price}}</span></div>
+                        @endif
                         <div class="stock-info in-stock">
                             <p class="availability">Availability: <b>{{$product->stock_status}}</b></p>
                         </div>
