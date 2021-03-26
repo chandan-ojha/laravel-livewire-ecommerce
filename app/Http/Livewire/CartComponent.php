936754697cb24f956c0f8e0932bf10c4c153867d
Chandan-Ojha <chandanojha776@gmail.com>
2021-03-26 19:06:01 +0600

    add SaveForLater option on cart page

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -7,6 +7,7 @@ use Cart;
 
 class CartComponent extends Component
 {
+    //increase cart quantity
     public function increaseQuantity($rowId)
     {
         $product = Cart::instance('cart')->get($rowId);
@@ -15,6 +16,7 @@ class CartComponent extends Component
         $this->emitTo('cart-count-component', 'refreshComponent');
     }
 
+    //decrease cart quantity
     public function decreaseQuantity($rowId)
     {
         $product = Cart::instance('cart')->get($rowId);
@@ -23,16 +25,46 @@ class CartComponent extends Component
         $this->emitTo('cart-count-component', 'refreshComponent');
     }
 
+    //remove cart product
     public function destroy($rowId)
     {
         Cart::instance('cart')->remove($rowId);
         $this->emitTo('cart-count-component', 'refreshComponent');
         session()->flash('success_message', 'Item has been removed');
     }
+
+    //remove cart all product
     public function destroyAll()
     {
         Cart::instance('cart')->destroy();
         $this->emitTo('cart-count-component', 'refreshComponent');
+    }
+
+    //Add product save for later
+    public function switchToSaveForLater($rowId)
+    {
+        $item = Cart::instance('cart')->get($rowId);
+        Cart::instance('cart')->remove($rowId);
+        Cart::instance('saveForLater')->add($item->id, $item->name, 1, $item->price)->associate('App\Models\Product');
+        $this->emitTo('cart-count-component', 'refreshComponent');
+        session()->flash('success_message', 'Item has been saved for later');
+    }
+
+    //move to cart
+    public function moveToCart($rowId)
+    {
+        $item = Cart::instance('saveForLater')->get($rowId);
+        Cart::instance('saveForLater')->remove($rowId);
+        Cart::instance('cart')->add($item->id, $item->name, 1, $item->price)->associate('App\Models\Product');
+        $this->emitTo('cart-count-component', 'refreshComponent');
+        session()->flash('s_success_message', 'Item has been moved to cart');
+    }
+
+    //delete product from save for later
+    public function deleteFromSaveForLater($rowId)
+    {
+        Cart::instance('saveForLater')->remove($rowId);
+        session()->flash('s_success_message', 'Item has been removed from save for later');
     }
 
     public function render()
--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -9,7 +9,6 @@
 			</ul>
 		</div>
 		<div class=" main-content-area">
-
 			<div class="wrap-iten-in-cart">
 				@if(Session::has('success_message'))
 				<div class="alert alert-success">
@@ -36,6 +35,7 @@
 								<a class="btn btn-increase" href="#" wire:click.prevent="increaseQuantity('{{$item->rowId}}')"></a>
 								<a class="btn btn-reduce" href="#" wire:click.prevent="decreaseQuantity('{{$item->rowId}}')"></a>
 							</div>
+							<p class="text-center"> <a href="#" wire:click.prevent="switchToSaveForLater('{{$item->rowId}}')"> Save For Later</a> </p>
 						</div>
 						<div class="price-field sub-total">
 							<p class="price">{{$item->subtotal}}</p>
@@ -73,6 +73,45 @@
 					<a class="btn btn-clear" href="#" wire:click.prevent="destroyAll()">Clear Shopping Cart</a>
 					<a class="btn btn-update" href="#">Update Shopping Cart</a>
 				</div>
+			</div>
+
+			<!--Product Saved For Later -->
+			<div class="wrap-iten-in-cart">
+				<h3 class="title-box" style="border-bottom: 1px solid; padding-bottom:15px;">{{Cart::instance('saveForLater')->count()}} item(s) Saved For Later</h3>
+				@if(Session::has('s_success_message'))
+				<div class="alert alert-success">
+					<strong>Success</strong> {{Session::get('s_success_message')}}
+				</div>
+				@endif
+				@if(Cart::instance('saveForLater')->count() > 0)
+				<h3 class="box-title">Products Name</h3>
+				<ul class="products-cart">
+					@foreach(Cart::instance('saveForLater')->content() as $item)
+					<li class="pr-cart-item">
+						<div class="product-image">
+							<figure><img src="{{ asset('assets/images/products') }}/{{$item->model->image}}" alt="{{$item->model->name}}"></figure>
+						</div>
+						<div class="product-name">
+							<a class="link-to-product" href="{{route('product.details',['slug'=>$item->model->slug])}}">{{$item->model->name}}</a>
+						</div>
+						<div class="price-field produtc-price">
+							<p class="price">{{$item->model->regular_price}}</p>
+						</div>
+						<div class="quantity">
+							<p class="text-center"> <a href="#" wire:click.prevent="moveToCart('{{$item->rowId}}')"> Move To Cart</a> </p>
+						</div>
+						<div class="delete">
+							<a href="#" wire:click.prevent="deleteFromSaveForLater('{{$item->rowId}}')" class="btn btn-delete" title="">
+								<span>Delete from save for later</span>
+								<i class="fa fa-times-circle" aria-hidden="true"></i>
+							</a>
+						</div>
+					</li>
+					@endforeach
+				</ul>
+				@else
+				<p>No item saved for later</p>
+				@endif
 			</div>
 
 			<div class="wrap-show-advance-info-box style-1 box-in-site">
