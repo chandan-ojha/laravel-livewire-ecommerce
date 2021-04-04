1da6bac95b3a5d9ee7092527dc7719a32cac2cc8
Chandan-Ojha <chandanojha776@gmail.com>
2021-04-04 23:23:05 +0600

    admin apply coupons

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Coupon;
 use Livewire\Component;
 use Cart;
 
 class CartComponent extends Component
 {
+    public $haveCouponCode;
+    public $couponCode;
+    public $discount;
+    public $subtotalAfterDiscount;
+    public $taxAfterDiscount;
+    public $totalAfterDiscount;
+
     //increase cart quantity
     public function increaseQuantity($rowId)
     {
@@ -67,8 +75,53 @@ class CartComponent extends Component
         session()->flash('s_success_message', 'Item has been removed from save for later');
     }
 
+    //Apply Coupon Code
+    public function applyCouponCode()
+    {
+        $coupon = Coupon::where('code', $this->couponCode)->where('cart_value', '<=', Cart::instance('cart')->subtotal())->first();
+        if (!$coupon) {
+            session()->flash('coupon_message', 'Coupon code is invalid');
+            return;
+        }
+
+        session()->put('coupon', [
+            'code' => $coupon->code,
+            'type' => $coupon->type,
+            'value' => $coupon->value,
+            'cart_value' => $coupon->cart_value
+        ]);
+    }
+
+    //Discount Calculation
+    public function calculateDiscounts()
+    {
+        if (session()->has('coupon')) {
+            if (session()->get('coupon')['type'] == 'fixed') {
+                $this->discount = session()->get('coupon')['value'];
+            } else {
+                $this->discount = (Cart::instance('cart')->subtotal() * session()->get('coupon')['value']) / 100;
+            }
+            $this->subtotalAfterDiscount = Cart::instance('cart')->subtotal() - $this->discount;
+            $this->taxAfterDiscount = ($this->subtotalAfterDiscount * config('cart.tax')) / 100;
+            $this->totalAfterDiscount = $this->subtotalAfterDiscount + $this->taxAfterDiscount;
+        }
+    }
+
+    //Remove Coupon
+    public function removeCoupon()
+    {
+        session()->forget('coupon');
+    }
+
     public function render()
     {
+        if (session()->has('coupon')) {
+            if (Cart::instance('cart')->subtotal() < session()->get('coupon')['cart_value']) {
+                session()->forget('coupon');
+            } else {
+                $this->calculateDiscounts();
+            }
+        }
         return view('livewire.cart-component')->layout("layouts.base");
     }
 }
--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -58,14 +58,41 @@
 				<div class="order-summary">
 					<h4 class="title-box">Order Summary</h4>
 					<p class="summary-info"><span class="title">Subtotal</span><b class="index">${{Cart::instance('cart')->subtotal()}}</b></p>
+					@if(Session::has('coupon'))
+					<p class="summary-info"><span class="title">Discount ({{Session::get('coupon')['code']}}) <a href="#" wire:click.prevent="removeCoupon"><i class="fa fa-times text-danger"></i></a></span><b class="index"> -${{number_format($discount,2)}}</b></p>
+					<p class="summary-info"><span class="title">Subtotal with Discount </span><b class="index">${{number_format($subtotalAfterDiscount,2)}}</b></p>
+					<p class="summary-info"><span class="title">Tax ({{config('cart.tax')}}%) </span><b class="index">${{number_format($taxAfterDiscount,2)}}</b></p>
+					<p class="summary-info total-info "><span class="title">Total</span><b class="index">${{number_format($totalAfterDiscount,2)}}</b></p>
+
+					@else
 					<p class="summary-info"><span class="title">Tax</span><b class="index">${{Cart::instance('cart')->tax()}}</b></p>
 					<p class="summary-info"><span class="title">Shipping</span><b class="index">Free Shipping</b></p>
 					<p class="summary-info total-info "><span class="title">Total</span><b class="index">${{Cart::instance('cart')->total()}}</b></p>
+					@endif
+
 				</div>
+
 				<div class="checkout-info">
+					@if(!Session::has('coupon'))
 					<label class="checkbox-field">
-						<input class="frm-input " name="have-code" id="have-code" value="" type="checkbox"><span>I have promo code</span>
+						<input class="frm-input " name="have-code" id="have-code" value="1" type="checkbox" wire:model="haveCouponCode"><span>I have coupon code</span>
 					</label>
+					@if($haveCouponCode == 1)
+					<div class="summary-item">
+						<form wire:submit.prevent="applyCouponCode">
+							<h4 class="title-box">Coupon Code</h4>
+							@if(Session::has('coupon_message'))
+							<div class="alert alert-danger" role="danger">{{Session::get('coupon_message')}}</div>
+							@endif
+							<p class="row-in-form">
+								<label for="coupon-code">Enter your coupon code:</label>
+								<input type="text" name="coupon-code" wire:model="couponCode" />
+							</p>
+							<button type="submit" class="btn btn-small">Apply</button>
+						</form>
+					</div>
+					@endif
+					@endif
 					<a class="btn btn-checkout" href="checkout.html">Check out</a>
 					<a class="link-to-shop" href="shop.html">Continue Shopping<i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
 				</div>
