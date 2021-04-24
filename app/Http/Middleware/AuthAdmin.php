5b2872c732b38685a48c391e5601e53c713380fa
Chandan-Ojha <chandanojha776@gmail.com>
2021-04-24 22:06:45 +0600

    Solution for admin route not working after stripe integration

--- app/Http/Middleware/AuthAdmin.php
+++ app/Http/Middleware/AuthAdmin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthAdmin
 {
@@ -16,7 +17,7 @@ class AuthAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        if (session('utype') === 'ADM') {
+        if (Auth::user()->utype === 'ADM') {
             return $next($request);
         } else {
             session()->flush();
