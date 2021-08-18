fecd9e47bdde667fe9b17883db8e25dfd7211f3e
Chandan-Ojha <chandanojha776@gmail.com>
2021-08-18 16:21:38 +0600

    Show Processing Message When Placing an Order

--- resources/views/livewire/checkout-component.blade.php
+++ resources/views/livewire/checkout-component.blade.php
@@ -22,7 +22,7 @@
 			</ul>
 		</div>
 		<div class=" main-content-area">
-			<form wire:submit.prevent="placeOrder">
+			<form wire:submit.prevent="placeOrder" onsubmit="$('#processing').show();">
 				<div class="row">
 					<div class="col-md-12">
 						<div class="wrap-address-billing">
@@ -206,6 +206,14 @@
 						@if(Session::has('checkout'))
 						<p class="summary-info grand-total"><span>Grand Total</span> <span class="grand-total-price">${{Session::get('checkout')['total']}}</span></p>
 						@endif
+
+						@if($errors->isEmpty())
+						<div wire:ignore id="processing" style="font-size:22px; margin-bottom:20px; padding-left:37px; color:green; display:none;">
+							<i class="fa fa-spinner fa-pulse fa-fw"></i>
+							<span>Processing...</span>
+						</div>
+						@endif
+
 						<button type="submit" class="btn btn-medium">Place order now</button>
 					</div>
 					<div class="summary-item shipping-method">
