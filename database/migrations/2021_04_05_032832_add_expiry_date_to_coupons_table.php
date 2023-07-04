196798a0557fb6e77ee3a9958d31157b986f2683
Chandan Ojha <chandanojha776@gmail.com>
2023-07-04 12:35:09 +0600

    `updated`

--- database/migrations/2021_04_05_032832_add_expiry_date_to_coupons_table.php
+++ database/migrations/2021_04_05_032832_add_expiry_date_to_coupons_table.php
@@ -15,7 +15,8 @@ class AddExpiryDateToCouponsTable extends Migration
     public function up()
     {
         Schema::table('coupons', function (Blueprint $table) {
-            $table->date('expiry_date')->default(DB::raw('CURRENT_DATE'));
+            //$table->date('expiry_date')->default(DB::raw('CURRENT_DATE'))->nullable();
+            $table->date('expiry_date')->nullable();
         });
     }
 
@@ -27,7 +28,7 @@ class AddExpiryDateToCouponsTable extends Migration
     public function down()
     {
         Schema::table('coupons', function (Blueprint $table) {
-            $table->date('expiry_date')->default(DB::raw('CURRENT_DATE'));
+            $table->date('expiry_date');
         });
     }
 }
