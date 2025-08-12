ae19e3f856f835117b93754771b8a4a2560d73d0
Chandan Ojha <chandanojha776@gmail.com>
2025-08-12 20:43:24 +0600

    Update base.blade.php

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -55,8 +55,8 @@
                         <div class="topbar-menu left-menu">
                             <ul>
                                 <li class="menu-item">
-                                    <a title="Hotline: (+123) 456 789" href="#"><span
-                                            class="icon label-before fa fa-mobile"></span>Hotline: (+123) 456 789</a>
+                                    <a title="Hotline: 01764805692" href="#"><span
+                                            class="icon label-before fa fa-mobile"></span>Hotline: 01764805692</a>
                                 </li>
                             </ul>
                         </div>
@@ -68,39 +68,6 @@
                                                 src="{{ asset('assets/images/lang-en.png') }}"
                                                 alt="lang-en"></span>English<i class="fa fa-angle-down"
                                             aria-hidden="true"></i></a>
-                                    <ul class="submenu lang">
-                                        <li class="menu-item"><a title="hungary" href="#"><span
-                                                    class="img label-before"><img
-                                                        src="{{ asset('assets/images/lang-hun.png') }}"
-                                                        alt="lang-hun"></span>Hungary</a></li>
-                                        <li class="menu-item"><a title="german" href="#"><span
-                                                    class="img label-before"><img
-                                                        src="{{ asset('assets/images/lang-ger.png') }}"
-                                                        alt="lang-ger"></span>German</a></li>
-                                        <li class="menu-item"><a title="french" href="#"><span
-                                                    class="img label-before"><img
-                                                        src="{{ asset('assets/images/lang-fra.png') }}"
-                                                        alt="lang-fre"></span>French</a></li>
-                                        <li class="menu-item"><a title="canada" href="#"><span
-                                                    class="img label-before"><img
-                                                        src="{{ asset('assets/images/lang-can.png') }}"
-                                                        alt="lang-can"></span>Canada</a></li>
-                                    </ul>
-                                </li>
-                                <li class="menu-item menu-item-has-children parent">
-                                    <a title="Dollar (USD)" href="#">Dollar (USD)<i class="fa fa-angle-down"
-                                            aria-hidden="true"></i></a>
-                                    <ul class="submenu curency">
-                                        <li class="menu-item">
-                                            <a title="Pound (GBP)" href="#">Pound (GBP)</a>
-                                        </li>
-                                        <li class="menu-item">
-                                            <a title="Euro (EUR)" href="#">Euro (EUR)</a>
-                                        </li>
-                                        <li class="menu-item">
-                                            <a title="Dollar (USD)" href="#">Dollar (USD)</a>
-                                        </li>
-                                    </ul>
                                 </li>
 
                                 <!-- Login and Registration -->
