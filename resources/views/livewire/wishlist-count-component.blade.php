f9014783eebc9705cc62c796ada0132e26e710df
Chandan Ojha <chandanojha776@gmail.com>
2025-08-16 20:42:53 +0600

    Update wishlist-count-component.blade.php

--- resources/views/livewire/wishlist-count-component.blade.php
+++ resources/views/livewire/wishlist-count-component.blade.php
@@ -1,9 +1,10 @@
 <div class="wrap-icon-section wishlist">
-    <a href="{{route('product.wishlist')}}" class="link-direction">
-        <i class="fa fa-heart" aria-hidden="true"></i>
+    <a href="{{ route('product.wishlist') }}" class="link-direction">
+        <i class="fa fa-heart" style="{{ Cart::instance('wishlist')->count() > 0 ? 'color: #ff7007 !important;' : '' }}"
+            aria-hidden="true"></i>
         <div class="left-info">
-            @if(Cart::instance('wishlist')->count() > 0)
-                <span class="index">{{Cart::instance('wishlist')->count()}} item</span>
+            @if (Cart::instance('wishlist')->count() > 0)
+                <span class="index">{{ Cart::instance('wishlist')->count() }} item</span>
             @endif
             <span class="title">Wishlist</span>
         </div>
